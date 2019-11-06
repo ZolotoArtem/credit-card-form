ba9bae41415420bc3c93fa6bf0b8fb53dfed18bd
Золото Артем Леонидович <Zoloto.A.Le@sberbank.ru>
2019-11-06 17:19:22 +0300

    fix

--- src/components/cardpayservice/CardPayService.js
+++ src/components/cardpayservice/CardPayService.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTransition, animated } from 'react-spring';
-import { Transition } from 'react-spring/renderprops';
 
 import { ReactComponent as MastercardLogo } from './images/mastercard.svg';
 import { ReactComponent as MirLogo } from './images/mir.svg';
@@ -16,7 +15,6 @@ const services = {
 const AnimatedWrapper = animated(Wrapper);
 
 export const CardPayService = ({ service }) => {
-    // const props = useSpring({ to: { marginTop: -20 }, from: { marginTop: 20 } });
     const transitions = useTransition(service, service, {
         from: { transform: 'translate3d(0,-40px,0)', opacity: 0 },
         enter: { transform: 'translate3d(0,0px,0)', opacity: 1 },
@@ -27,12 +25,4 @@ export const CardPayService = ({ service }) => {
             {services[item]}
         </AnimatedWrapper>
     ));
-    // </Transition>
-
-    // return (
-    //     <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
-    //         {props => <Wrapper style={props}>{services[service]}</Wrapper>}
-    //     </Spring>
-    // );
-    // return <AnimatedWrapper style={props}>{services[service]}</AnimatedWrapper>;
 };
